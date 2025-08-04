a87f61bb98f7a69488379ab3ed7a517d007aed5f
Shaina G <shainagtherapy@gmail.com>
2025-08-03 18:18:28 -0700

    Postman routes created and first order success!

--- controllers/orders.js
+++ controllers/orders.js
@@ -4,15 +4,21 @@ const router = express.Router();
 const Order = require('../models/order.js')
 
 router.post('/', async (req, res) => {
-    console.log(req.body);
-    const order = await Order.create(req.body);
+    try{
+        const newOrder = await Order.create(req.body);
+        res.json(newOrder);
+    } catch (error) {
+        console.log(error)
+    }
+})
 
-        
-
-  
-
-    res.json(order);
-
+router.get('/orders', async (req, res) => {
+    try {
+        const orders = await Order.find({})
+        res.render('orders/index.ejs', {orders})
+    } catch (error) {
+        console.log(error)
+    }
 })
 
 module.exports = router;
--- models/order.js
+++ models/order.js
@@ -1,7 +1,42 @@
 const mongoose = require('mongoose');
 
 const orderSchema = new mongoose.Schema({
-  name: String
+  customerName: { type: String, required: true },
+  customerPhone: { type: String },
+  customerEmail: { type: String },
+  orderDate: { type: Date },
+  desiredPickupDate: { type: Date },
+  cheeseboard: { type: String, required: true }, // or you could reference a Menu model
+  quantity: { type: Number, required: true },
+  jam: {
+    type: String,
+    enum: ['fig', 'apricot', 'cherry'],
+    default: 'None'
+  },
+  chocolate: {
+    type: String,
+    enum: ['dark', 'milk', 'vegan'],
+    default: 'None'
+  },
+  nuts: {
+    type: String,
+    enum: ['marcona almonds', 'pistachios', 'candied pecans'],
+    default: 'None'
+  },
+  carbs: {
+    type: String,
+    enum: ['baguette', 'rosemary crackers', 'sourdough crackers', 'gluten-free'],
+    default: 'None'
+  },
+  notes:{
+  notes: String,
+  },
+  status: {
+    type: String,
+    enum: ['enquiry', 'confirmed', 'complete'],
+    default: 'Enquiry'
+  },
+  
 });
 
 const Order = mongoose.model('Order', orderSchema);
